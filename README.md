865131b28c9b93eac81d230b2be3ca7f3a7df348
rahuldcoder <rahulthakur02k@gmail.com>
2018-01-05 17:49:42 +0530

    Wikipedia Web Scrapper

---
+++ README.md
@@ -0,0 +1,23 @@
+# web_scraper_wikipedia
+This is the code for the "Wikipedia Web Scraper" which gives frequency percentage of top 100 wikipedia words from a wikipedia article based on a given key_word entered by user. 
+
+#Installation
+
+The necessary dependencies are in the requirements.txt file so just run this before running the actual code to get them installed
+
+``
+pip install -r requirements.txt
+``
+
+#Usage
+
+There are two arguments. The first is the article you want to retrive words from. The second is a boolean value that describes
+whether or not you want to remove stop words. 
+
+``
+python main.py your_article_name_here yes
+``
+
+#Credits
+
+Thanks to Siraj Raval for the inspiration
---
+++ main.py
@@ -0,0 +1,118 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import operator
+import json
+from tabulate import tabulate
+import sys
+from stop_words import get_stop_words
+import pprint
+
+def getWordList(url):
+    print(url)
+    word_list = []
+    
+    source_code = requests.get(url)
+    plain_text = source_code.text
+    file=open('source_code.html','w+')
+    file.write(plain_text)
+    file.close()
+    soup = BeautifulSoup(plain_text,'html.parser')
+
+    file=open('content.txt','w+')
+    for text in soup.findAll('p'):
+        if text.text is None:
+            continue
+        
+        content = text.text
+        file.write(content+'\n')
+    
+        words = content.lower().split()
+
+    
+        for word in words:
+            cleaned_word = clean_word(word)
+            if len(cleaned_word) > 0:
+                word_list.append(cleaned_word)
+
+    return word_list
+
+
+def clean_word(word):
+    cleaned_word = re.sub('[^A-Za-z]+', '', word)
+    return cleaned_word
+
+
+def createFrquencyTable(word_list):
+    word_count = {}
+    for word in word_list:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+
+    return word_count
+
+def remove_stop_words(frequency_list):
+    stop_words = get_stop_words('en')
+
+    temp_list = []
+    for key,value in frequency_list:
+        if key not in stop_words:
+            temp_list.append([key, value])
+
+    return temp_list
+
+wikipedia_api_link = "https://en.wikipedia.org/w/api.php?format=json&action=query&list=search&srsearch="
+wikipedia_link = "https://en.wikipedia.org/wiki/"
+
+if(len(sys.argv) < 2):
+    print("Enter valid string")
+    exit()
+
+string_query = sys.argv[1]
+
+if(len(sys.argv) > 2):
+    search_mode = True
+else:
+    search_mode = False
+
+url = wikipedia_api_link + string_query
+
+try:
+  
+    response = requests.get(url)
+
+ 
+    data = json.loads(response.content.decode("utf-8"))
+
+ 
+    wikipedia_page_tag = data['query']['search'][0]['title']
+
+    url = wikipedia_link + wikipedia_page_tag
+    page_word_list = getWordList(url)
+    page_word_count = createFrquencyTable(page_word_list)
+    sorted_word_frequency_list = sorted(page_word_count.items(), key=operator.itemgetter(1), reverse=True)
+    if(search_mode):
+        sorted_word_frequency_list = remove_stop_words(sorted_word_frequency_list)
+
+    total_words_sum = 0
+    for key,value in sorted_word_frequency_list:
+        total_words_sum = total_words_sum + value
+
+    if len(sorted_word_frequency_list) > 100:
+        sorted_word_frequency_list = sorted_word_frequency_list[:100]
+
+
+    final_list = []
+    for key,value in sorted_word_frequency_list:
+        percentage_value = float(value * 100) / total_words_sum
+        final_list.append([key, value, round(percentage_value, 4)])
+
+    print_headers = ['Word', 'Frequency', 'Frequency Percentage']
+
+    print(tabulate(final_list, headers=print_headers, tablefmt='orgtbl'))
+
+except requests.exceptions.Timeout:
+    print("The server didn't respond. Please, try again later.")
+
